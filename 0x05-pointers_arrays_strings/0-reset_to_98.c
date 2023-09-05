2b665ef90b95cf68d76a3b56914e01856c5a59c9
Godwin Essien <Godwinessien001@gmail.com>
2023-09-05 01:22:38 -0700

    0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*  reset_to_98 - takes the pointer and updates the value
+*  @n: int to check
+*  Owned By Godwin Essien
+*  Return: 0 if  successful
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
