d42c99ad9708948c78b349f34445aeefd4f6e00e
Godwin Essien <Godwinessien001@gmail.com>
2023-09-14 11:15:04 -0700

    02-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
-/*
+/**
  *main - prints the name of the file
  *
  *Return: always 0
 */
 
-#include <stdio.h>
-
 int main(void)
 {
-    printf("Name of source file: %s\n", __FILE__);
-
-    return 0;
+	printf("%s\n", __FILE__);
+	return (0);
 }
