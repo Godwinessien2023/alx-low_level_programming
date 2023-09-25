65365c89be86b66cc444d1e911a80ec20b09fa62
Godwinessien2023 <kinggodwin4u@yahoo.com>
2023-09-25 08:51:15 -0700

    Mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,8 +2,8 @@
 
 /**
  * _print - moves a string one place to the left and prints the string
- * str: string to move
- * l: size of string
+ * @str: string to move
+ * @l: size of string
  * Return: void
  */
 void _print(char *str, int l)
@@ -26,11 +26,11 @@ void _print(char *str, int l)
 
 /**
  * mul - multiplies a char with a string and places the answer into dest
- * n: char to multiply
- * num: string to multiply
- * num_index: last non NULL index of num
- * dest: destination of multiplication
- * dest_index: highest index to start addition
+ * @n: char to multiply
+ * @num: string to multiply
+ * @num_index: last non NULL index of num
+ * @dest: destination of multiplication
+ * @dest_index: highest index to start addition
  * Return: pointer to dest, or NULL on failure
  */
 char *mul(char n, char *num, int num_index, char *dest, int dest_index)
@@ -61,7 +61,7 @@ char *mul(char n, char *num, int num_index, char *dest, int dest_index)
 
 /**
  * check_for_digits - checks the arguments to ensure they are digits
- * av: pointer to arguments
+ * @av: pointer to arguments
  * Return: 0 if digits, 1 if not
  */
 int check_for_digits(char **av)
@@ -81,9 +81,8 @@ int check_for_digits(char **av)
 
 /**
  * init - initializes a string
- * str: sting to initialize
- * l: length of strinf
- *
+ * @str: sting to initialize
+ * @l: length of strinf
  * Return: void
  */
 void init(char *str, int l)
@@ -97,8 +96,8 @@ void init(char *str, int l)
 
 /**
  * main - multiply two numbers
- * argc: number of arguments
- * argv: argument vector
+ * @argc: number of arguments
+ * @argv: argument vector
  * Return: zero, or exit status of 98 if failure
  */
 
