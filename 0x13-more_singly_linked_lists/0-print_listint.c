55ed284976d44adcc813d1ad0224446852bc45c0
Godwinessien2023 <kinggodwin4u@yahoo.com>
2023-09-26 19:08:18 -0700

    Print Listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,18 +5,20 @@
  * @h: head of linklist node
  * Return: the number of nodes
  */
+
 size_t print_listint(const listint_t *h)
 {
-	int count = 0;
-	
-	if (h != NULL)
-	{
-		while (h)
-		{
-			printf("%d\n", h->n);
-			h = h->next;
-			count++;
-		}
-	}
-	return (count);
+    int count = 0;
+
+    if (h != NULL)
+    {
+        while (h)
+        {
+            printf("%d\n", h->n);
+            h = h->next;
+            count++;
+        }
+    }
+    return (count);
 }
+
