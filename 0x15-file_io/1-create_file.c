66225dbb75cf86ef4c7faf51d5e56be2c52a56c5
Godwinessien2023 <kinggodwin4u@yahoo.com>
2023-10-08 11:41:13 -0700

    File i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,10 +2,10 @@
 
 
 /**
- * create_file - creates an array of chars, and initializes.
+ * create_file- creates an array of chars, and initializes.
  * @text_content: this is a NULL terminated string to write to the file.
  * @filename: this is the name of the file to create.
- * return: 1 on success, -1 if it fails
+ * Return:1 on success, -1 if it fails
  */
 
 int create_file(const char *filename, char *text_content)
